cee73d2395b017725c13b2526e725a2b82c45c6f
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2024-03-12 01:28:17 +0100

    [Client/ErrorPopup] Simplify layout
    Since the message is static a ViewModel is overkill

--- Client/ErrorPopup.axaml.cs
+++ Client/ErrorPopup.axaml.cs
@@ -9,8 +9,8 @@ public partial class ErrorPopup : Window
 {
 	public ErrorPopup(string msg)
 	{
-		DataContext = new ErrorPopupViewModel(msg);
 		InitializeComponent();
+		MessageBlock.Text = msg;
 		Width = Program.config.width / 2;
 		Height = Program.config.height / 2;
 		Topmost = true;
@@ -19,27 +19,5 @@ public partial class ErrorPopup : Window
 	private void CloseClick(object? sender, RoutedEventArgs args)
 	{
 		Close();
-	}
-}
-public class ErrorPopupViewModel(string msg) : INotifyPropertyChanged
-{
-	private string message = msg;
-	public string Message
-	{
-		get => message;
-		set
-		{
-			if(value != message)
-			{
-				message = value;
-				NotifyPropertyChanged();
-			}
-		}
-	}
-
-	public event PropertyChangedEventHandler? PropertyChanged;
-	private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
-	{
-		PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 	}
 }
