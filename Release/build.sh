06bfb1ce06cc37b6942b36244a60e14a4e983cca
0x4261756D <38735823+0x4261756D@users.noreply.github.com>
2024-04-11 19:12:23 +0200

    [Release] Actually include picture related stuff

--- Release/build.sh
+++ Release/build.sh
@@ -11,6 +11,8 @@ echo "$name"
 mkdir -p $name
 cp -r config "$name/"
 cp -r ../Core/decks "$name/"
+mkdir -p "$name/pictures"
+cp ../Client/pictures/default_artwork.png "$name/pictures"
 mkdir -p "$name/Core"
 cd ../Client/
 dotnet publish -c Release -r linux-x64 --sc true /p:PublishAot=true
